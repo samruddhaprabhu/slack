6f3398bfa087ce290295a5508eff5b27fc2dca37
DungeonMaster976 <38967050+DungeonMaster976@users.noreply.github.com>
2020-11-23 22:02:46 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDQh7v-3xMnItRHn1mM19x6WhXRYTJmMAQ",
+    authDomain: "slack-e9dd3.firebaseapp.com",
+    databaseURL: "https://slack-e9dd3.firebaseio.com",
+    projectId: "slack-e9dd3",
+    storageBucket: "slack-e9dd3.appspot.com",
+    messagingSenderId: "649677042345",
+    appId: "1:649677042345:web:dd9ded1e9b17d089f12fb8",
+    measurementId: "G-96T53FN7CJ"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.fireStore();
