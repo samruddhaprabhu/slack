41e399665146396144f4886f6b3f450f2267b4d8
DungeonMaster976 <38967050+DungeonMaster976@users.noreply.github.com>
2020-11-24 12:41:53 +0530

    first commit

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Chat.css";
 import {useParams} from "react-router-dom";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "./firebase";
+import Message from './Message';
 
 function Chat() {
     const {roomId} = useParams();
+    const [roomDetails, setRoomDetails] = useState(null);
+    const [roomMessages, setRoomMessages] = useState([]);
+    
+    useEffect(() => {
+        if(roomId) {
+            db.collection('rooms')
+            .doc(roomId)
+            .onSnapshot(snapshot => (setRoomDetails(snapshot.data())
+            ))
+        }
+
+        db.collection('rooms')
+          .doc(roomId)
+          .collection('messages')
+          .orderBy('timestamp', 'asc')
+          .onSnapshot((snapshot) =>
+              setRoomMessages(snapshot.docs.map(doc => doc.data())));
+    }, [roomId]);
+
     return (
         <div className="chat">
-            <h2>You are in the {roomId} room</h2>
             <div className="chat_header">
                 <div className="chat_headerLeft">
                     <h4 className="chat_channelName">
-                        <strong># general</strong>
+                        <strong>#{roomDetails?.name}</strong>
                         <StarBorderOutlinedIcon/>
                     </h4>
                 </div>
@@ -21,6 +41,17 @@ function Chat() {
                         <InfoOutlinedIcon/> Details
                     </p>
                 </div>
+            </div>
+
+            <div className="chat_messages">
+                {roomMessages.map(({message, timestamp, user, userimage}) => (
+                    <Message
+                    message={message}
+                    timestamp={timestamp}
+                    user={user}
+                    userimage={userimage}
+                    />
+                ))}
             </div>
         </div>
     )
